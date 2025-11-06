d652e704817c2f405bf3d7d94a974cd77f962db1
reethuo <152209084+reethuo@users.noreply.github.com>
2025-11-06 15:43:42 +0530

    Update delegate.tf

--- delegate.tf
+++ delegate.tf
@@ -31,7 +31,7 @@ module "delegate" {
 
   account_id = "ucHySz2jQKKWQweZdXyCog"
   delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
-  delegate_name = "terraform-delegate"
+  delegate_name = "terraform-delegate-helm-reethu"
   deploy_mode = "KUBERNETES"
   namespace = "harness-delegate-ng"
   manager_endpoint = "https://app.harness.io"
